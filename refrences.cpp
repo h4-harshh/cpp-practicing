9def532a5f0ed10d0a3fead15ad0ce7c778bd5f7
h4-harshh <harshnew2204@gmail.com>
2023-01-19 09:52:28 +0530

    refrencing strign in cpp using vector

---
+++ refrences.cpp
@@ -0,0 +1,31 @@
+#include<bits/stdc++.h>
+#include<string>
+using namespace std;
+
+
+
+int main()
+{
+    vector<string> str{"geeksforgeeks practice","geeksforgeeks ide","geeksforgeeks write"};
+    
+    // for(int i=0;i<str.length();i++)
+    // {
+    //     cout<<str[i];
+    // }
+    // cout<<endl;
+
+    for(auto x:str)
+    cout<<x<<" ";
+
+    cout<<endl;
+
+    for(auto &x:str)
+    cout<<x<<" ";
+
+    cout<<endl;
+
+    for(const auto &x:str)
+    cout<<x<<" ";
+
+    return 0;
+}
